b671d6075da0a8c373a884dfb74f82bf8cd43375
Marco <lefollmarc@hotmail.fr>
2014-07-10 09:48:25 +0200

    add contact page with test

--- spec/features/static_pages_spec.rb
+++ spec/features/static_pages_spec.rb
@@ -2,6 +2,8 @@ require 'rails_helper'
 
 describe "Static pages" do
 
+  let(:base_title) { "Ruby on rails Tutorial Sample App |" }
+
   describe "Home page" do
 
     it "should have the h1 'Sample App'" do
@@ -13,7 +15,7 @@ describe "Static pages" do
       visit '/static_pages/home'
       expect(page).to have_selector('title',
       :visible => false,
-      :text => "Ruby on rails Tutorial Sample App | Home")
+      :text => "#{base_title} Home")
     end
   end
 
@@ -28,7 +30,7 @@ describe "Static pages" do
       visit '/static_pages/help'
       expect(page).to have_selector('title',
       :visible => false,
-      :text => "Ruby on rails Tutorial Sample App | Help")
+      :text => "#{base_title} Help")
     end
   end
 
@@ -43,7 +45,22 @@ describe "Static pages" do
       visit '/static_pages/about'
       expect(page).to have_selector('title',
       :visible => false,
-      :text => "Ruby on rails Tutorial Sample App | About")
+      :text => "#{base_title} About")
+    end
+
+    describe "Contact page" do
+
+      it "should have the content 'Contact'" do
+        visit '/static_pages/contact'
+        expect(page).to have_selector('h1', :text => 'Contact')
+      end
+
+      it "should have the right title 'Contact'" do
+        visit '/static_pages/contact'
+        expect(page).to have_selector('title',
+          :visible => false,
+          :text => "#{base_title} Contact")
+      end
     end
   end
 end
